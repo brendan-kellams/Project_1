4ac3e1b41d21d11f6619eb5dca845d0200734d8f
nguyenhachuy <nguyenhachuy@gmail.com>
2017-10-14 13:59:40 -0700

    Search now populates well

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -7,12 +7,18 @@ $(".search-form").submit((event) => {
 		headers: {
 			Authorization: "Bearer " + localStorage.getItem("token")
 		},
-		url: "https://api.spotify.com/v1/albums/0sNOF9WDwhWunNAHPD3Baj",
+		url: "https://api.spotify.com/v1/search",
 		method: "GET",
 		data: {
+			q : input,
+			type: "track"
 		},
 
 	}).done((response) => {
 		console.log(response);
+		response.tracks.items.forEach((value, index, array) => {
+
+			$("#media").append($("<div>").text(value.name));
+		});
 	});
 });
