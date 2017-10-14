e4d7c1e10cc3288b24819b31f505e291bef82268
Majid <tomajid@yahoo.com>
2017-10-14 14:15:01 -0700

    created music app firebase

--- public/javascript/javascript.js
+++ public/javascript/javascript.js
@@ -1,18 +1,3 @@
-//====================================================================
-// Firebase Initialization
-//====================================================================
-var config = {
-    apiKey: "AIzaSyAyCExyUKAi8ZxurGIdXMOI12fekQA-2AM",
-    authDomain: "musicapp-e8c67.firebaseapp.com",
-    databaseURL: "https://musicapp-e8c67.firebaseio.com",
-    projectId: "musicapp-e8c67",
-    storageBucket: "",
-    messagingSenderId: "317115906434"
-};
-firebase.initializeApp(config);
-var database = firebase.database();
-
-
 //=====================================================================
 // This is used to trigger the toggle button to toggle the sidebar menu
 //=====================================================================
@@ -29,53 +14,6 @@ $('#sidebarCollapse').on('click', function () {
     } else {
         $('iframe').css('width', '1210px');;
     }
-});
-
-
-//====================================================================
-//  displayUsers()
-//====================================================================
-function displayUsers(groupName, userName)
-{
- 	//$("#groupSubmenu").append("<li></li>" + userName);
- 	$("#groupSubmenu").append('<li class="groupList"><a href="#">'+groupName+'</a></li>');
- 	$("#friendSubmenu").append('<li class="collapse list-unstyled"><a href="#">'+userName+'</a></li>');
-}
-
-
-//====================================================================
-//  addUser()
-//====================================================================
-function addUser(){
-
-  event.preventDefault();
-
-  var userName = $("#user-name").val().trim();
-  var groupName = $("#group-name").val().trim();
-  
-  database.ref('/groups').push({
-        username: userName,
-        groupname: groupName,
-        date: firebase.database.ServerValue.TIMESTAMP
-  });
-
-  $("#user-name").val("");
-  $("#group-name").val("");
-}
-
-
-//====================================================================
-//  database.ref()
-//====================================================================
-database.ref('/groups').on("child_added", function(childSnapshot, prevChildKey) {
-
-  console.log(childSnapshot.val());
-
-  // Store everything into a variable.
-  var groupName = childSnapshot.val().groupname;
-  var userName = childSnapshot.val().username;
-
-  displayUsers(groupName, userName);
 });
 
 
