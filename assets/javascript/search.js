1386db047dc266e869e7a155a7a6b1c7ba74648c
Gus Nguyen <nguyenhachuy@gmail.com>
2017-10-15 13:02:50 -0700

    Making more changes

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -59,9 +59,28 @@ $(".search-form").submit((event) => {
 
 function addSong() {
 	console.log('Song added');
-	console.log($(this).data('id'));
+	var id = $(this).data('id');
 	$('.song-list').remove();
 	$('.playlist').show();
+	var url = "https://api.spotify.com/v1/users/" + "1298427285" + "/playlists/" + "5Cgnluzrv0uQ7mVcrtYSSb"
+	+ "/tracks?";
+	var track = "uris=spotify:track" + id;
+	url += track;
+	$.ajax({
+		headers: {
+			Authorization: "Bearer " + localStorage.getItem("token"),
+		},
+		url: url,
+		method: "POST",
+		data: {
+			uris: [
+				track
+			]
+
+		}
+
+	}).done((response) => {
+	});
 }
 
 $(document).on('click', '.song-button', addSong);
