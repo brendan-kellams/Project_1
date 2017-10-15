338315a53ec7dbbe7ec5667c8b98ad9365ccb71e
Gus Nguyen <nguyenhachuy@gmail.com>
2017-10-15 11:38:26 -0700

    Search now works really well

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,3 +1,4 @@
+
 $(".search-form").submit((event) => {
 	event.preventDefault();
 	var input = $("#search").val();
@@ -15,10 +16,41 @@ $(".search-form").submit((event) => {
 		},
 
 	}).done((response) => {
+		$(".playlist").hide();
+		var list = $('<ul>').addClass('song-list media-list');
+
 		console.log(response);
 		response.tracks.items.forEach((value, index, array) => {
-
-			$("#media").append($("<div>").text(value.name));
+			var song = $('<li>').addClass('media');
+			var img_div = $('<div>').addClass('media-left');
+			var img = $('<img>').addClass('media-object').attr('alt', 'Generic placeholder image');
+			img_div.append(img);
+			var body = $('<div>').addClass('song-body media-body');
+			var title = $('<h5>').addClass('mt-0 mb-1 media-heading');
+			img.attr('src', value.album.images[2].url);
+			title.text(value.name);
+			body.text(value.artists[0].name);
+			body.append(title);
+			song.append(img_div, body);
+			list.append(song);
+			// $("#media").append($("<div>").text(value.name));
 		});
+		$("#media").append(list);
 	});
 });
+
+// $("#contact").on("click", (event) => {
+// 	console.log(localStorage.getItem("token"));
+// 	$.ajax({
+// 		headers: {
+// 			Authorization: "Bearer " + localStorage.getItem("token")
+// 		},
+// 		url: "https://api.spotify.com/v1/me/playlists",
+// 		method: "GET",
+// 		data: {
+// 		},
+
+// 	}).done((response) => {
+// 		console.log(response);
+// 	});
+// });
