5f753a7e7cafacf62aa9768701f5f439ff1481b4
Brendan Kellams <brendan.kellams@gmail.com>
2017-10-18 13:08:19 -0700

    Here are the changes to the media and header sizing

--- public/javascript/javascript.js
+++ public/javascript/javascript.js
@@ -8,17 +8,6 @@ $(document).ready(function () {
         $('#sidebar').toggleClass('active');
     });
 });
-var collapsed = true;
-// this is used to reshape the contents when hitting the toggle button
-$('#sidebarCollapse').on('click', function () {
-    if (collapsed) {
-        $('iframe').css('width', '1141px');
-    } else {
-        $('iframe').css('width', '1318px');
-    }
-    collapsed = !collapsed;
-    
-});
 
 // the modal
 $('#myModal').on('shown.bs.modal', function () {
