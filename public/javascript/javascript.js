b57ca401a95ee0883f6f14d32a5d658e690d2d0a
Brendan Kellams <brendan.kellams@gmail.com>
2017-10-17 20:36:26 -0700

    Here is the updated UI

--- public/javascript/javascript.js
+++ public/javascript/javascript.js
@@ -1,21 +1,29 @@
 //=====================================================================
 // This is used to trigger the toggle button to toggle the sidebar menu
 //=====================================================================
+
 $(document).ready(function () {
+    $('#sidebar').toggleClass('active');
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
     });
 });
-
+var collapsed = true;
 // this is used to reshape the contents when hitting the toggle button
 $('#sidebarCollapse').on('click', function () {
-    if ($('iframe').width() === '700px') {
-        $('iframe').css('width', '800px');
+    if (collapsed) {
+        $('iframe').css('width', '1080px');
     } else {
-        $('iframe').css('width', '700px');
+        $('iframe').css('width', '1150px');
     }
+    collapsed = !collapsed;
+    
 });
 
+// the modal
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').focus()
-  });
+});
+
+
+// like counter
